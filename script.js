4b03841a91785c5954cbef9d6fee0316fc9d6b95
Paulienomial <54091492+Paulienomial@users.noreply.github.com>
2022-10-06 16:14:18 +0200

    Layout done and rocket moves with WASD or Arrow Keys

---
+++ script.js
@@ -0,0 +1,33 @@
+function addListener(){
+    const rocket = document.getElementById("UserRocket");
+    rocket.style.top = "45vh";
+    rocket.style.left = "47vw";
+    window.addEventListener('keydown', (event) => 
+    {
+        console.log(event.key);
+        if((event.key == 'ArrowUp') || (event.key == 'w')){
+            newVal = parseInt(rocket.style.top) - 1;
+            rocket.style.top =  newVal + "vh";
+            rocket.style.transform = "rotate(0deg)";
+        }else{
+            if((event.key == 'ArrowDown') || (event.key == 's')){
+                newVal = parseInt(rocket.style.top) + 1;
+                rocket.style.top = newVal + "vh";
+                rocket.style.transform = "rotate(180deg)";
+            }else{
+                if((event.key == 'ArrowLeft') || (event.key == 'a')){
+                    newVal = parseInt(rocket.style.left) - 1;
+                    rocket.style.left = newVal + "vw";
+                    rocket.style.transform = "rotate(270deg)";
+                }else{
+                    if((event.key == 'ArrowRight') || (event.key == 'd')){
+                        newVal = parseInt(rocket.style.left) + 1;
+                        rocket.style.left = newVal + "vw";
+                        rocket.style.transform = "rotate(90deg)";
+                    }
+                }
+            }
+        }
+
+    })
+}
